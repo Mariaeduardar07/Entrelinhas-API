5b3c5cedd783ed17298820cb4ef5c0519ce79a59
Maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2025-09-23 16:05:14 -0300

    feat: add prisma configuration, implement control, model and routes for authors

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import { config } from "dotenv";
 import cors from "cors"; // Importa o middleware CORS
 
+import routes from "./routes/index.routes.js";
+
 config(); // Carrega variáveis de ambiente do arquivo .env
 const port = process.env.PORT || 5000;
 
@@ -11,10 +13,15 @@ app.use(cors()); // Habilita CORS para todas as rotas
 
 app.use(express.json()); // Parse de JSON
 
+// Middleware para servir arquivos estáticos (imagens)
+app.use('/uploads', express.static('uploads'));
+
 // Rota base para verificar se o servidor está rodando
 app.get("/", (req, res) => {
   res.json({ message: "API de autores e livros funcionando! 📖" });
 });
+
+app.use("/", routes);
 
 // Iniciar o servidor
 app.listen(port, () => {
